22edee33c66db7b6ad299dd8fa39a7470dfb40f2
afd <afd@alup5.academiapostal.es.local>
2020-07-16 19:49:17 +0200

    proyectosjs

--- api/proyectos.js
+++ api/proyectos.js
@@ -6,10 +6,20 @@ function CrearProyecto(req, res) {
 
     })
     .catch(err =>{
-        res.status(400).json(err)
+        res.status(400).json(err.message)
     })
     
 }
+ function listarProyectos(req, res) {
+     Proyectos.findAll()
+     .then(proyectos => {
+         res.status(200).json(proyectos)
+     })
+     .catch(err=>{
+         res.status(400).json(err.message)
+     })
+ }
+ 
 module.exports = {
     CrearProyecto
     //añadir mas funciones a medida que se vayan haciendo
